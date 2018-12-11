1e299e437336e07fb4ee01685867b4b7883dde7c
RameshBettge <bettge.r@gmx.de>
2018-12-11 13:59:13 +0100

    CHANGED: unlitSea.shader
    Haven't necessarily made improvements but wanted to save the work before removing anything

---
+++ Assets/_Scripts/Sea/SetLightDir.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetLightDir : MonoBehaviour {
+
+    [SerializeField]
+    Material seaMat;
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        Vector3 fwd = transform.forward;
+        seaMat.SetVector("LightDir", new Vector4(fwd.x, fwd.y, fwd.z, 0f));
+	}
+}
