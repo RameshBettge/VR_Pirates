2bf45c4bfd6306346d8264fe4b0960d21d5b465e
clina <celina.wuschko@gmail.com>
2018-12-14 11:03:51 +0100

    particleSystemsAdded

--- Assets/_Scripts/PCSpieler/Target.cs
+++ Assets/_Scripts/PCSpieler/Target.cs
@@ -2,6 +2,8 @@
 
 public class Target : MonoBehaviour
 {
+    public GameObject fireEffect;
+    public GameObject waterSplash;
     public float health = 500;
     bool walkingWasTriggered = false;
 
@@ -15,12 +17,17 @@ public class Target : MonoBehaviour
                 walkingWasTriggered = true;
                 other.GetComponentInParent<EnemyBehaiviour>().Walking();
                 other.GetComponentInParent<MoveOnPath>().speed = 2.5f;
-                //set an particleEffect later
+                Instantiate(fireEffect, transform.position, transform.rotation);
             }
             if (health <= 0)
             {
                 Destroy(gameObject);
             }
+        }
+
+        if (other.gameObject.tag == "Bucket")
+        {
+            Instantiate(waterSplash, transform.position, transform.rotation);
         }
     }
 }
