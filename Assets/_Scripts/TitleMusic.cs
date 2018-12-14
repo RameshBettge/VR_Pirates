d9e93bc3e2e3546ffb5e66147b122987c0828d09
RameshBettge <bettge.r@gmx.de>
2018-12-14 23:13:26 +0100

    Changed: Menu Scene

---
+++ Assets/_Scripts/TitleMusic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+public class TitleMusic : MonoBehaviour
+{
+
+    int currentSceneIndex;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        CheckScene();
+    }
+
+
+    void CheckScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+        if(scene.buildIndex == currentSceneIndex) { return; }
+
+        currentSceneIndex = scene.buildIndex;
+
+        if (scene.name == "FULL GAME")
+        {
+            Destroy(this);
+            Destroy(gameObject);
+        }
+    }
+
+}
