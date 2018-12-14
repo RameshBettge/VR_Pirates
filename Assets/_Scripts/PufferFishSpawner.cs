e912e0ba1ae7556023ec678ab4e94a02e7b76013
RameshBettge <bettge.r@gmx.de>
2018-12-14 12:04:30 +0100

    ADDED: Pufferfish spawner

---
+++ Assets/_Scripts/PufferFishSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PufferFishSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pufferFishPrefab;
+
+    Vector3 pufferOffset = new Vector3(1.7467f, -0.16686f, 3.1605f);
+
+    Transform[] spawnPoints;
+
+    float force = 30f;
+
+    float spawnInterval = 10f;
+
+    float nextSpawn;
+
+    void Start()
+    {
+        spawnPoints = GetComponentsInChildren<Transform>();
+        SpawnPuffer();
+
+        nextSpawn = spawnInterval;
+    }
+
+    void Update()
+    {
+        if(Time.time >= nextSpawn)
+        {
+            SpawnPuffer();
+            nextSpawn = Time.time + spawnInterval;
+        }
+    }
+
+    void SpawnPuffer()
+    {
+        int spawnNum = UnityEngine.Random.Range(0, spawnPoints.Length);
+
+        GameObject puffer = Instantiate(pufferFishPrefab, spawnPoints[spawnNum].position /*+ pufferOffset*/, Quaternion.identity, transform.parent);
+        Rigidbody rb = puffer.GetComponent<Rigidbody>();
+        rb.AddForce(spawnPoints[spawnNum].forward * force, ForceMode.VelocityChange);
+
+        //puffer.transform.position = spawnPoints[spawnNum].position + pufferOffset;
+
+        Vector3 randomRot = Vector3.zero;
+        for (int i = 0; i < 3; i++)
+        {
+            randomRot[i] = UnityEngine.Random.Range(-300f, 300f);
+        }
+
+        rb.AddTorque(randomRot);
+
+    }
+
+    void SetCorrectPos(Transform puffer, Vector3 pos)
+    {
+
+    }
+}
---
+++ Assets/_Scripts/ResetLocalPos.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetLocalPos : MonoBehaviour
+{
+
+    private void LateUpdate()
+    {
+        transform.localPosition = Vector3.zero;
+    }
+}
