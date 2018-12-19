4b7efc23ff546aa59de672d1e885a788a42ae0dc
RameshBettge <bettge.r@gmx.de>
2018-12-19 13:33:39 +0100

    ADDED: Script which sets fullScreenMode

---
+++ Assets/_Scripts/ForceWindowed.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceWindowed : MonoBehaviour
+{
+    public FullScreenMode screenMode;
+
+    private void Awake()
+    {
+        Screen.fullScreenMode = screenMode;
+    }
+
+}
